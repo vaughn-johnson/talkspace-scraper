6edeb75e28005ac27c568523664c45472f5e3c0c
Vaughn Johnson <vneck@uw.edu>
2020-10-27 15:54:31 -0700

    added message scraping

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 import axios from 'axios';
-console.log(`Scraping the data for ${process.env.USERNAME} from Talkspace`);
-
 
 const authenticate = async () => {
   const body = { userType: 'CLIENT' };
 
-  const token = Buffer.from(`${process.env.USERNAME}:${process.env.PASSWORD}`, 'utf8')
-                  .toString('base64')
+  const encodedCreds = Buffer.from(`${process.env.USERNAME}:${process.env.PASSWORD}`, 'utf8')
+                         .toString('base64')
   const options = {
-    headers: { Authorization: `Basic ${token}` }
+    headers: { Authorization: `Basic ${encodedCreds}` }
   };
 
-  return await axios.post(process.env.AUTH_URL, body, options);
+  const res = await axios.post(process.env.AUTH_URL, body, options);
+  const { userID, access: token } = res.data.data;
+  return { userID, token };
 };
 
-console.log((await authenticate()).data);
+// TODO: Allow user to specificy particular room
+const getRoomId = async (userID, token) => {
+  const url = `https://clientapi.talkspace.com/v2/clients/${userID}/rooms/last-messages`;
+  const options = {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+
+  const res = await axios.get(url, options);
+
+  return res.data.data[0].roomId;
+};
+
+const scrapeMessages = async () => {
+  console.log(`Scraping the data for ${process.env.USERNAME} from Talkspace`);
+  const { userID, token } = await authenticate();
+
+  const body = {
+    params: {
+      "tid": await getRoomId(userID, token),
+      "lastMessageId": 0,
+      "limit": 2 ** 10, // TODO: guarantee scrape all messages
+      "loadMediaURLs": true
+    }
+  };
+
+  const options = {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+
+  const res = await axios.post(process.env.API_URL, body, options)
+
+  return res.data['result '].messages;
+};
+
+console.log(await scrapeMessages());
