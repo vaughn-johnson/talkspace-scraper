470a1753ef017156bd89450372e0b54c8a5da0a1
Vaughn Johnson <vneck@uw.edu>
2020-10-28 12:37:57 -0700

    - Added transpilation in order to run tests - Added tests for error/ - fixed associated bugs

---
+++ error/error.test.js
@@ -0,0 +1,9 @@
+import { errorMessage } from './index.js'
+
+describe('errorMessage', () => {
+  const messages = [[undefined], [null], ['error'], ['e'.repeat(2 ** 5)]];
+  test.each(messages)(
+    "matches snapshot with message %p",
+    (msg) => expect(errorMessage(msg)).toMatchSnapshot()
+  )
+})
--- error/index.js
+++ error/index.js
@@ -1,4 +1,4 @@
-const errorMessage = (msg) => {
+export const errorMessage = (msg) => {
   // This is what we're going for
   //
   // =========
@@ -7,10 +7,10 @@ const errorMessage = (msg) => {
 
   const paddingLength = 2;
   const padding = ' '.repeat(paddingLength);
-  const { length } = msg;
+  const { length } = `${msg}`
 
   // Make a sandwich
-  const bread = '='.repeat((length + 2 * (paddingLength + 1)));
+  const bread = '='.repeat((length + 2 * (paddingLength + 2)));
   const meat = `==${padding}${msg}${padding}==`;
   return `\n${bread}\n${meat}\n${bread}\n`;
 };
