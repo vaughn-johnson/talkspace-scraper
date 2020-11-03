ee5affb5c78667faf3bee19d0b8c8417ca78e389
Vaughn Johnson <vneck@uw.edu>
2020-11-02 16:47:50 -0800

    fixed npm import /export

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { scrapeMessages, authenticate} from './talkspace/index.js';
 import saveMessagesToDB from './mongo/index.js';
 import logError from './error/index.js';
 
-module.exports = {
+export default {
  scrapeMessages,
   authenticate,
   saveMessagesToDB,
