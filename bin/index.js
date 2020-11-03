fc3d4c3d99a1e5059e9a96e090af941495cd9c52
vaughn-johnson <vneck@uw.edu>
2020-11-03 11:30:26 -0800

    linting

--- bin/index.js
+++ bin/index.js
@@ -3,10 +3,10 @@
 import { program } from 'commander';
 import dotenv from 'dotenv';
 import fs from 'fs';
+import readlineSync from 'readline-sync';
 import { scrapeMessages, authenticate } from '../talkspace/index.js';
 import saveMessagesToDB from '../mongo/index.js';
 import logError from '../error/index.js';
-import readlineSync from 'readline-sync';
 
 dotenv.config();
 
@@ -24,14 +24,23 @@ const scrape = async () => {
     overwrite, outputFile,
   } = program;
 
+  // Username
   const usernamePrompt = 'What is your Talkspace username / email? > ';
-  const username = program.username || process.env.USERNAME || readlineSync.question(usernamePrompt); 
+  const username = program.username
+    || process.env.USERNAME
+    || readlineSync.question(usernamePrompt);
 
-  const passwordPrompt = 'What is your Talkspace password? > '
-  const password = process.env.PASSWORD || readlineSync.question(passwordPrompt, { hideEchoBack: true });
-  
-  const mongoPrompt = `If you'd like to use MongoDB, paste a connection string. If not, leave blank> `;
-  const connectionString = process.env.MONGO_CONNECTION_STRING || readlineSync.question(mongoPrompt, { hideEchoBack: true }); 
+  // Password
+  const passwordPrompt = 'What is your Talkspace password? > ';
+  const password = process.env.PASSWORD
+    || readlineSync.question(passwordPrompt, { hideEchoBack: true });
+
+  // Connection String
+  const mongoPrompt = 'If you\'d like to use MongoDB, paste a connection string. '
+                    + 'If not, leave blank> ';
+  const connectionString = process.env.MONGO_CONNECTION_STRING
+    || readlineSync.question(mongoPrompt, { hideEchoBack: true });
+
 
   try {
     const messages = await scrapeMessages(
