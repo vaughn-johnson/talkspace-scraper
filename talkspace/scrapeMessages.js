1432246f9111fafc8dca3791fbf834a7f83a9e5a
Vaughn Johnson <v@ughn.io>
2020-11-08 16:31:46 -0800

    Make requests to entire history
    check to beginning of history
    updated tests

--- talkspace/scrapeMessages.js
+++ talkspace/scrapeMessages.js
@@ -5,16 +5,14 @@ import authenticate from './authenticate.js';
 export const API_URL = 'https://www.talkspace.com/rest/ios/method/getAllRepliesForThread';
 
 const scrapeMessages = async (authentication) => {
-  // await is not supported as a default param
   try {
+    // await is not supported as a default param
     const { token, roomID } = authentication || await authenticate();
     console.log(`Scraping the data for ${process.env.USERNAME} from Talkspace`);
 
-    const body = {
+    let body = {
       params: {
         tid: roomID,
-        lastMessageId: 0,
-        limit: 2 ** 10, // TODO: guarantee scrape all messages
         loadMediaURLs: true,
       },
     };
@@ -23,8 +21,22 @@ const scrapeMessages = async (authentication) => {
       headers: { Authorization: `Bearer ${token}` },
     };
 
-    const res = await axios.post(API_URL, body, options);
-    const { messages } = res.data['result '];
+    const messages = [];
+
+    let res = await axios.post(API_URL, body, options);
+
+    while (res.data['result '].messages.length > 0) {
+      const { messages: newMessages } = res.data['result '];
+      messages.push(...newMessages);
+
+      // This is actually the minimum id, because we want prior messages
+      const maxMessageId = newMessages.map((m) => m.message_id)
+        .reduce((a, b) => (a < b ? a : b));
+      body = { params: { ...body.params, maxMessageId } };
+
+      // eslint-disable-next-line no-await-in-loop
+      res = await axios.post(API_URL, body, options);
+    }
 
     console.log(`Successfully scraped ${messages.length} messages`);
     return messages;
--- talkspace/scrapeMessages.test.js
+++ talkspace/scrapeMessages.test.js
@@ -8,7 +8,10 @@ jest.mock('axios');
 jest.mock('./authenticate.js');
 jest.mock('../error/index.js');
 
-const messages = 'I wanted electricity to go everywhere in the world'.split(' ').map((m) => ({ m }));
+const messages = 'I wanted electricity to go everywhere in the world'.split(' ').map((m, index) => ({
+  message: m,
+  message_id: index,
+}));
 const error = { message: 'whoops', code: 500 };
 
 describe('scrapeMessages', () => {
@@ -17,11 +20,19 @@ describe('scrapeMessages', () => {
     /* eslint-disable  global-require */
     const { API_URL } = require('./scrapeMessages.js');
 
-    axios.post.mockImplementation((endpoint) => {
+    axios.post.mockImplementation((endpoint, body) => {
       if (endpoint === API_URL) {
+        const { params: { maxMessageId } } = body;
+
+        let filterMessages = messages;
+
+        if (maxMessageId >= 0) {
+          filterMessages = messages.filter((m) => m.message_id < maxMessageId);
+        }
+
         return {
           data: {
-            'result ': { messages },
+            'result ': { messages: filterMessages },
           },
         };
       }
